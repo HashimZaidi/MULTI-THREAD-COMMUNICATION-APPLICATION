dfdc0439b220390c825e784ebc23f10c759190a2
Syed Muhammad Hashim <hashim.muhammad9@gmail.com>
2022-01-10 18:14:11 +0500

    Added initial release version

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 import setuptools
 
 with open("requirements.txt", "r") as f:
-    required = f.read().splitlines(
-)
+    required = f.read().splitlines()
     setuptools.setup(
         name="messaging-app",
-        version="0.0.1"
+        version="0.0.1",
         author="Muhammad Hashim",
         author_email="hashim.muhammad9@gmail.com",
         packages=setuptools.find_packages(),
